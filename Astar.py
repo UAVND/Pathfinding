ca71f269b97ed2bf97a6c1b754a3737515146eec
Zach Vincent <vincent_z@delbarton.org>
2022-01-25 17:06:20 -0500

    made PathGUI and Astar runnable together PathGUI now a module implemented within Astar Astar runs w/o errors but doesn't function properly

--- Astar.py
+++ Astar.py
@@ -1,6 +1,6 @@
 # Zach Vincent
 # A* pathfinding
-# Last updated 1/22/2022
+# Last updated 1/25/2022
 
 # Add starting node to frontier
 # While goal isn't found:
@@ -14,7 +14,12 @@
         # Trace through nodes and their parents to find path
 
 import math
-from pprint import pprint
+import PathGUI as gui
+
+debug = True
+def dlog(string):
+    if debug:
+        print(string)
 
 maze = [['S', ' ', ' ', ' ', ' ', 'X', 'X'],
         ['X', 'X', 'X', 'X', ' ', 'X', 'X'],
@@ -23,17 +28,23 @@ maze = [['S', ' ', ' ', ' ', ' ', 'X', 'X'],
         [' ', 'X', ' ', 'X', ' ', ' ', ' '],
         ['F', 'X', ' ', ' ', ' ', 'X', 'X']]
 
+
+goal = (5, 0)
+
 class Node():
     def __init__(self, parent, coords):
         self.parent = parent
         self.coords = coords
-        self.x = coords[0]
-        self.y = coords[1]
-        self.neighbors = self.getNeighbors
-        self.heuristic = self.manhattan
+        self.x = coords[1]
+        self.y = coords[0]
+        self.neighbors = self.getNeighbors()
+        self.heuristic = self.manhattan()
+
+    def __str__(self):
+        return ('Neighbors for this node: ' + str(self.neighbors))
 
     def manhattan(self):
-        return goal.x-self.x + goal.y-self.y
+        return goal[1]-self.x + goal[0]-self.y
 
     def euclidean(self):
         return math.sqrt((goal.x-self.x)**2 + (goal.y-self.y)**2)
@@ -49,23 +60,28 @@ class Node():
     def isGoal(self):
         return self.coords == goal.coords
 
-class StackFrontier():
+class Frontier():
     def __init__(self):
         self.frontier = []
-        self.explored = {}
+        self.explored = set()
 
     def addNode(self, node: Node):
         self.frontier.append(node)
         self.explored.add(node.coords)
 
     def removeNode(self) -> Node:
-        return self.frontier.pop(-1)
+        return self.frontier.pop(self.frontier.index(self.shortestDistance()))
 
     def getLength(self):
         return len(self.frontier)
 
-class QueueFrontier(StackFrontier):
-    pass
+    def shortestDistance(self):
+        shortest = self.frontier[0]
+        for node in self.frontier:
+            if node.heuristic < shortest.heuristic:
+                shortest = node
+
+        return shortest
 
 def isValid(coordinates):
     if (coordinates[0] < len(maze) and coordinates[1] < len(maze[0])):
@@ -80,22 +96,45 @@ def printMaze():
             print(char, end=' ')
         print('')
 
+def tracePath(endNode):
+    node = endNode
+    path = []
+    while node.parent != None:
+        path.append(node.coords)
+        node = node.parent
+
+    return path[::-1]
+
 origin =  Node(None, (0, 0))
-goal = Node(None, (10, 10))
-#StackFrontier.addNode(goal)
+frontier = Frontier()
+frontier.addNode(origin)
 goalFound = False
+currentNode = None
 
 printMaze()
 
 while not goalFound:
-    # Pop first node
-    currentNode = StackFrontier.removeNode()
+    # Pop node (will be the node that is shortest distance to the goal thru function in Frontier)
+    currentNode = frontier.removeNode()
 
-    if currentNode.coords == goal.coords:
+    if currentNode.coords == goal:
         goalFound = True
     else:
         # Add all neighbors in node of interest
+        dlog('checkpoint 1')
+        print(currentNode)
         for neighbor in currentNode.neighbors:
-            if neighbor.coords not in StackFrontier.explored:
-                if isValid(neighbor.coords):
-                    StackFrontier.addNode(neighbor)
+            if neighbor not in frontier.explored:
+                dlog('c2')
+                if isValid(neighbor):
+                    dlog('c3')
+                    new = Node(currentNode, neighbor)
+                    frontier.addNode(new)
+
+path = tracePath(currentNode)
+print('Path:', path)
+
+for loc in path:
+    maze[loc[0]][loc[1]] = 'O'
+
+gui.initialize(maze)
--- PathGUI.py
+++ PathGUI.py
@@ -5,38 +5,31 @@ import random
 window_width = 500
 window_height = 500
 
-grid = [[' ', 'X', ' ', ' ', 'X', ' ', 'X', ' ', 'X', ' ', ' ', 'X', ' ', 'X', ' '],
-        ['X', 'X', 'X', ' ', 'X', 'X', ' ', ' ', 'X', ' ', ' ', 'X', ' ', 'X', ' '],
-        [' ', ' ', ' ', 'X', ' ', ' ', ' ', 'X', 'X', ' ', ' ', 'X', ' ', 'X', ' '],
-        [' ', ' ', 'X', ' ', 'X', 'X', ' ', ' ', 'X', ' ', ' ', 'X', ' ', 'X', ' '],
-        ['X', 'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', ' ', 'X', ' ', 'X', ' '],
-        [' ', 'X', 'X', ' ', ' ', 'X', 'X', 'X', 'X', ' ', ' ', 'X', ' ', 'X', ' '],
-        [' ', ' ', ' ', ' ', 'X', 'X', 'X', ' ', 'X', ' ', ' ', 'X', ' ', 'X', ' '],
-        [' ', 'X', ' ', ' ', 'X', ' ', 'X', ' ', 'X', ' ', ' ', 'X', ' ', 'X', ' '],
-        ['X', 'X', 'X', ' ', 'X', 'X', ' ', ' ', 'X', ' ', ' ', 'X', ' ', 'X', ' '],
-        [' ', ' ', ' ', 'X', ' ', ' ', ' ', 'X', 'X', ' ', ' ', 'X', ' ', 'X', ' '],
-        [' ', ' ', 'X', ' ', 'X', 'X', ' ', ' ', 'X', ' ', ' ', 'X', ' ', 'X', ' '],
-        ['X', 'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', ' ', 'X', ' ', 'X', ' '],
-        [' ', 'X', 'X', ' ', ' ', 'X', 'X', 'X', 'X', ' ', ' ', 'X', ' ', 'X', ' '],
-        [' ', ' ', ' ', ' ', 'X', 'X', 'X', ' ', 'X', ' ', ' ', 'X', ' ', 'X', ' ']]
-
-def createObstacle():
+def createObstacle(grid, canvas):
     randx = random.randint(0, len(grid[0])-1)
     randy = random.randint(0, len(grid)-1)
+
+    while(grid[randy][randx] == 'X'):
+        randx = random.randint(0, len(grid[0])-1)
+        randy = random.randint(0, len(grid)-1)
 
     grid[randy][randx] = 'X'
-    buildGrid()
-    print('created random obstacle')
+    buildGrid(grid, canvas)
+    print('Created random obstacle')
 
-def deleteObstacle():
+def deleteObstacle(grid, canvas):
     randx = random.randint(0, len(grid[0])-1)
     randy = random.randint(0, len(grid)-1)
 
+    while(grid[randy][randx] == ' '):
+        randx = random.randint(0, len(grid[0])-1)
+        randy = random.randint(0, len(grid)-1)
+
     grid[randy][randx] = ' '
-    buildGrid()
+    buildGrid(grid, canvas)
     print('Removed random obstacle')
 
-def buildGrid():
+def buildGrid(grid, canvas):
     box_width = 20
     box_height = 20
 
@@ -54,10 +47,15 @@ def buildGrid():
             endy = originy+box_height
 
             if i < len(grid) and j < len(grid[0]):
-                if(grid[i][j] == 'X'):
+                loc = grid[i][j]
+                if(loc == 'X'):
                     color = 'red'
-                else:
+                elif (loc == ' '):
                     color = 'gray'
+                elif (loc == 'O'):
+                    color = 'green'
+                else:
+                    color='yellow'
             else:
                 color = 'gray'
 
@@ -66,29 +64,34 @@ def buildGrid():
 
 
 # Initializing
-print('starting... ', end='')
 root = Tk()
+grid = []
 
-option_frame = Frame(root, width=100, height=100, bg='red')
-option_frame.pack(fill='x')
+def initialize(maze):
+    print('starting... ', end='')
+    grid = maze
 
-main_frame = Frame(root)
-main_frame.pack(fill='x')
+    option_frame = Frame(root, width=100, height=100, bg='red')
+    option_frame.pack(fill='x')
 
-canvas = Canvas(main_frame, width=window_width, height=window_height, bg='white')
-canvas.pack(fill='x')
+    main_frame = Frame(root)
+    main_frame.pack(fill='x')
 
-print('initialized')
+    canv = Canvas(main_frame, width=window_width, height=window_height, bg='white')
+    canv.pack(fill='x')
 
-algs = ['select algorithm', 'A*', 'Dijkstra', 'Elastic']
-selected_alg = StringVar()
+    #print('initialized')
 
-ttk.OptionMenu(option_frame, selected_alg, *algs).grid(column=0, row=0, padx=20)
-ttk.Button(option_frame, text='Quit', command=root.destroy).grid(column=1, row=0)
-ttk.Button(option_frame, text='Create random obstacle', command=createObstacle).grid(column=2,row=0, padx=10)
-ttk.Button(option_frame, text='Delete random obstacle', command=deleteObstacle).grid(column=3,row=0, padx=10)
+    algs = ['select algorithm', 'A*', 'Dijkstra', 'Elastic']
+    selected_alg = StringVar()
 
-buildGrid()
+    ttk.OptionMenu(option_frame, selected_alg, *algs).grid(column=0, row=0, padx=20)
+    ttk.Button(option_frame, text='Quit', command=root.destroy).grid(column=1, row=0)
+    ttk.Button(option_frame, text='Create random obstacle', command=lambda : createObstacle(maze, canv)).grid(column=2,row=0, padx=10)
+    ttk.Button(option_frame, text='Delete random obstacle', command=lambda : deleteObstacle(maze, canv)).grid(column=3,row=0, padx=10)
 
-print('running')
-root.mainloop()
+    buildGrid(maze, canv)
+
+    print('running')
+    root.mainloop()
+
