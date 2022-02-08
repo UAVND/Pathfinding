ed7d6935d8ecd320c17d6e19124d5e638572f987
Zach <exzachlyme@gmail.com>
2022-02-08 14:59:56 -0500

    builds 2D list matrix from GPS data

--- PathfindingGPS.py
+++ PathfindingGPS.py
@@ -1,13 +1,16 @@
 # Zach Vincent
 # Pathfinding with GPS coordinates
-# Last updated 1/26/2022
+# Last updated 2/8/2022
 # v0.1
 
-#import Astar as astar
+# Find slope between two points and simplify
+
+import Astar as astar
+#import PathGUI as gui
 import json
 
 # === PARAMS ===
-resolution = 10
+resolution = 25
 
 grid = [[' '] * resolution for x in range(resolution)]
 
@@ -44,45 +47,53 @@ def GPStoGrid(latitude, longitude, marker):
                 #print(floor_j and ceiling_j)
 
                 if ceiling_j and floor_j:
-                    print('marker placed')
+                    print('marker placed at ({}, {})'.format(i, j))
                     grid[i][j] = marker
 
-lat = []
-long = []
-obstacles = []
+# ===GET LONG/LAT LIST OF OBSTACLES===
+boundary_lat = []
+boundary_long = []
 
-for i in range(len(data['stationaryObstacles'])):
-    pos = data['stationaryObstacles'][i]
+boundary_points = data['flyZones'][0].get('boundaryPoints')
+
+for i in range(len(boundary_points)):
+    pos = boundary_points[i]
     latitude = pos.get('latitude')
     longitude = pos.get('longitude')
 
-    lat.append(latitude)
-    long.append(longitude)
+    boundary_lat.append(latitude)
+    boundary_long.append(longitude)
+
+# ===USE LONG/LAT LIST TO FIND LARGEST AND SMALLEST===
 
 # Latitude measures distance north/south of equator,
 # therefore latitude is a measure of y-axis and is
 # accessed through outer lists of grid array (i). deltaY
 # measures box height.
-minLat = min(lat)
-maxLat = max(lat)
+minLat = min(boundary_lat)
+maxLat = max(boundary_lat)
 deltaY = abs((maxLat-minLat)/resolution)
 
 # Longitude measures distance east/west of meridian,
 # therefore longitude is a measure of x-axis and is
 # accessed through inner lists of grid array (j). deltaX
 # measures box width.
-minLong = min(long)
-maxLong = max(long)
+minLong = min(boundary_long)
+maxLong = max(boundary_long)
 deltaX = abs((maxLong-minLong)/resolution)
 
 print('Lat: Min: {:.4f} | Max: {:.4f}'.format(minLat, maxLat))
 print('Lon: Min: {:.4f} | Max: {:.4f}'.format(minLong, maxLong))
 
-for i in range(len(data['stationaryObstacles'])):
-    pos = data['stationaryObstacles'][i]
+
+for i in range(len(boundary_points)):
+    pos = boundary_points[i]
     print('Lat: {:.4f} | Long: {:.4f}'.format(pos.get('latitude'), pos.get('longitude')))
 
     GPStoGrid(pos.get('latitude'), pos.get('longitude'), 'X')
+
+print('DeltaX:',deltaX)
+print('DeltaY:',deltaY)
 
 for row in grid:
     print(row)
